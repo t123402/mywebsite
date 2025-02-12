4261efa40b08d71c9e41df17df6ac0c2881735d1
hsuanju.chen@yesee.com.tw <hsuanju.chen@yesee.com.tw>
2025-02-12 14:17:05 +0800

    初始化履歷網站

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div style={{ textAlign: "center", padding: "50px" }}>
+			<h1>👋 嗨，我是小可！</h1>
+			<p>這是我的個人履歷網站 🚀</p>
+			<p>技能：React, Node.js, Security+, CCNA</p>
+			<p>
+				聯絡方式：<a href="mailto:你的Email">你的Email</a>
+			</p>
+		</div>
+	);
+}
+
+export default App;
